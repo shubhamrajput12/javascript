be8487def3f783fc9e257ffe4914d885259b679e
shubhamrajput12 <76531639+shubhamrajput12@users.noreply.github.com>
2024-04-30 17:51:06 +0000

    operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = null;
+
+// console.log(typeof(score));
+
+let intNumber = Number(score);
+
+// console.log(typeof intNumber);
+// console.log(intNumber);
+// console.log((NaN+1));
+
+// "33" => 33
+// "33acb" => NaN
+// true => 1; false => 0;
+
+let issBoolean = null;
+
+const value = Boolean(issBoolean);
+console.log(value);
+
+// 1 => true; 0 => false
+// "" => false;
+// "shubham" => true;
+
+let ankitNumber = 25;
+// console.log( typeof ankitNumber);
+// console.log(typeof (String(ankitNumber)));
+// console.log((String(ankitNumber) + 1));
+
+
+
+
+
+
+console.log(2+2+"1");
+console.log("2"+(2+1));
+console.log(typeof("2"+(2+1)));
+console.log(typeof(2+2+"1"));
+// "ba"+(typeof (Number("32aa")+"a"
+console.log("ba"+ (Number("32aa")+"a"))
