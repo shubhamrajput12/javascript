87624e82ff1d0ea7b57cad6c0a8a126ac269b275
shubhamrajput12 <76531639+shubhamrajput12@users.noreply.github.com>
2024-06-30 12:36:38 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,8 +14,8 @@ let intNumber = Number(score);
 
 let issBoolean = null;
 
-const value = Boolean(issBoolean);
-console.log(value);
+// const value = Boolean(issBoolean);
+// console.log(value);
 
 // 1 => true; 0 => false
 // "" => false;
@@ -31,9 +31,54 @@ let ankitNumber = 25;
 
 
 
-console.log(2+2+"1");
-console.log("2"+(2+1));
-console.log(typeof("2"+(2+1)));
-console.log(typeof(2+2+"1"));
+// console.log(2+2+"1");
+// console.log("2"+(2+1));
+// console.log(typeof("2"+(2+1)));
+// console.log(typeof(2+2+"1"));
 // "ba"+(typeof (Number("32aa")+"a"
-console.log("ba"+ (Number("32aa")+"a"))
+// console.log("ba"+ (Number("32aa")+"a"))
+
+
+
+
+
+
+// *********************** Ooperations ************************88
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+const str1 = "shubham";
+const str2 = " Rajput"
+const str3 = str1+str2;
+
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+2+"2");
+// console.log(1+2+"2");
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1=num2=num3 = 2+2  //this is bad wat of writing code i.e not market standards.
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+// prefix and postfix these two are different type of increamentals.
