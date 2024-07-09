b8afa6860f069959e371d7f43ed34ab627bef83a
shubhamrajput12 <76531639+shubhamrajput12@users.noreply.github.com>
2024-07-09 18:55:32 +0000

    memories heap and stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -13,7 +13,7 @@ let userEmail;
 const id = Symbol('123');
 const anotherId = Symbol('123');
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 // const bigNumber = 124123123412341234n;
@@ -32,4 +32,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive) , Heap (Non - primitive)
+
+let myYoutubeName = "shubham@google.com";
+ let anotherName = myYoutubeName;
+
+ anotherName = "chaiorcode";
+
+ console.log(myYoutubeName);
+ console.log(anotherName);
+
+ let userOne ={
+    email: "shubham@yopmail.com",
+    upi: "shubham@oksbi"
+ }
+ userTwo = userOne;
+
+userTwo.email = "shubham@google,com";
+console.log(userOne.email);
+console.log(userTwo.email);
+ 
