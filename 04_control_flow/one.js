34028e1831bcc1ea89d87df8b10f0b9cd30f5c99
shubhamrajput12 <76531639+shubhamrajput12@users.noreply.github.com>
2024-11-01 18:29:26 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,38 @@
+//if
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+
+// if(temperature === 41){
+//     console.log("temperature is less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly";
+//     console.log(`user power ${power}`);   
+// }
+
+// console.log(`user power ${power}`);   
+
+// const balance = 6000;
+
+// if (balance > 500) console.log("test");
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to buy courses");
+    
+}
